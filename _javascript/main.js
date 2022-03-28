284858802e503e1366fbb42e42eee6a8ec33a3ba
Zeljka <z.raic@adriamedia.hr>
2022-03-28 16:54:44 +0200

    all sections

--- _javascript/main.js
+++ _javascript/main.js
@@ -12,3 +12,20 @@ function mobileMenu() {
     navMenu.classList.toggle("active");
 }
 
+
+// linkovi
+let button = document.querySelectorAll('button');
+let content_inside = document.querySelectorAll('.content_inside');
+
+Array.from(button).forEach(function(buttonArray, i) {
+buttonArray.addEventListener('click', function() {
+
+    Array.from(button).forEach(buttonAll => buttonAll.classList.remove('button_active'));
+    
+    Array.from(content_inside).forEach(content_insideAll => content_insideAll.classList.remove('content_inside_active'));
+    
+    button[i].classList.add('button_active'); 
+    
+    content_inside[i].classList.add('content_inside_active');  
+  });
+});
