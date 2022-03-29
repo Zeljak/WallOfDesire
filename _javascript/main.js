b76291613e28eb11c26c0a7524711f4a4c97ee39
Zeljka <z.raic@adriamedia.hr>
2022-03-29 11:09:15 +0200

    footer

--- _javascript/main.js
+++ _javascript/main.js
@@ -13,19 +13,18 @@ function mobileMenu() {
 }
 
 
-// linkovi
-let button = document.querySelectorAll('button');
-let content_inside = document.querySelectorAll('.content_inside');
-
-Array.from(button).forEach(function(buttonArray, i) {
-buttonArray.addEventListener('click', function() {
-
-    Array.from(button).forEach(buttonAll => buttonAll.classList.remove('button_active'));
-    
-    Array.from(content_inside).forEach(content_insideAll => content_insideAll.classList.remove('content_inside_active'));
-    
-    button[i].classList.add('button_active'); 
-    
-    content_inside[i].classList.add('content_inside_active');  
-  });
+// Smooth Scrolling
+$(".links__style").on("click", function(event){
+  if (this.hash !== "") {
+      event.preventDefault();
+      const hash = this.hash;
+      $("html, body").animate(
+          {
+              scrollTop: $(hash).offset().top
+          },
+          1100
+      );
+  }
 });
+
+
